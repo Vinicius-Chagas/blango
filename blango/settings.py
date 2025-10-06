1587a56cf8044e2c1b6c7085213df53a6d57fc25
Vinícius Cândido <vinicandido00@gmail.com>
2025-10-06 02:05:01 +0000

    Finish crispy forms

--- blango/settings.py
+++ blango/settings.py
@@ -34,6 +34,7 @@ CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = 'None'
 SESSION_COOKIE_SAMESITE = 'None'
+CRISPY_ALLOWED_TEMPLATE_PACK = "bootstrap5"
 
 
 # Application definition
@@ -45,7 +46,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog'
+    'blog',
+    'crispy_forms',
+    'crispy_bootstrap5'
 ]
 
 MIDDLEWARE = [
