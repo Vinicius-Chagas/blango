534a27bee95a6eef978a5190042b422152f90ecc
Vinicius Vinicius <vinicandido00@gmail.com>
2025-11-14 17:53:16 +0000

    Finish Serializers

--- blango/settings.py
+++ blango/settings.py
@@ -78,6 +78,7 @@ class Dev(Configuration):
         "allauth.account",
         "allauth.socialaccount",
         "allauth.socialaccount.providers.google",
+        "rest_framework"
     ]
 
     MIDDLEWARE = [
